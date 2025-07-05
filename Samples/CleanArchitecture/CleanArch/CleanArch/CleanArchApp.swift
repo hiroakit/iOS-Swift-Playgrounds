ac7680af988227dbe3a5d84edc64dcc25a37a082
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2025-07-05 10:40:12 +0900

    Initial commit on this branch

---
+++ Samples/CleanArchitecture/CleanArch/CleanArch/CleanArchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchApp.swift
+//  CleanArch
+//
+//  Created by hiroakit on 2025/07/05.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Samples/CleanArchitecture/CleanArch/CleanArchTests/CleanArchTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchTests.swift
+//  CleanArchTests
+//
+//  Created by hiroakit on 2025/07/05.
+//
+
+import Testing
+@testable import CleanArch
+
+struct CleanArchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
